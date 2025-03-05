577ab8d61b00f391edd347906987f8edcbd3406d
Erdembileg Ariunbold <97537483+Zwique@users.noreply.github.com>
2025-03-05 10:06:57 -0500

    Update leaky_vault.c

--- leaky_vault.c
+++ leaky_vault.c
@@ -1,39 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-#define BUFSIZE 32
-#define FLAGSIZE 64
 
 void win() {
-  char buf[FLAGSIZE];
-  FILE *f = fopen("flag.txt","r");
-  if (f == NULL) {
-    printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
-    exit(0);
-  }
-
-  fgets(buf,FLAGSIZE,f);
-  printf(buf);
+    char flag[100];
+    FILE *fp = fopen("flag.txt", "r");
+    if (fp == NULL) {
+        printf("Flag file not found!\n");
+        exit(1);
+    }
+    fgets(flag, sizeof(flag), fp);
+    printf("%s", flag);
+    fclose(fp);
 }
 
-void vuln(){
-  char buf[BUFSIZE];
-  gets(buf);
-
-  printf("Okay, time to return... Fingers Crossed... Jumping to 0x%x\n", get_return_address());
+void vuln() {
+    char buffer[64];
+    printf("Enter your input: ");
+    gets(buffer);
+    printf("You entered: %s\n", buffer);
 }
 
-int main(int argc, char **argv){
-
-  setvbuf(stdout, NULL, _IONBF, 0);
-
-  gid_t gid = getegid();
-  setresgid(gid, gid, gid);
-
-  puts("Please enter your string: ");
-  vuln();
-  return 0;
+int main() {
+    vuln();
+    return 0;
 }
