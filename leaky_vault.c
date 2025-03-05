9d060a052c83f4345757a013834f7dad73c5a476
Erdembileg Ariunbold <97537483+Zwique@users.noreply.github.com>
2025-03-05 09:57:15 -0500

    Update leaky_vault.c

--- leaky_vault.c
+++ leaky_vault.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "asm.h"
 
-char* gets(char* str);
+#define BUFSIZE 32
+#define FLAGSIZE 64
 
-void secret_function() {
-    printf("Congratulations! You found the secret function!\n");
-    printf("Here's your flag: uacCTF{m3m9ry_l2ak_3xpl0it_CS1410}\n");
+void win() {
+  char buf[FLAGSIZE];
+  FILE *f = fopen("flag.txt","r");
+  if (f == NULL) {
+    printf("Flag File is Missing. Problem is Misconfigured, please contact an Admin if you are running this on the shell server.\n");
+    exit(0);
+  }
+
+  fgets(buf,FLAGSIZE,f);
+  printf(buf);
 }
 
-void vault() {
-    char buffer[64];
-    printf("Welcome to the Leaky Vault!\n");
-    printf("Enter your name: ");
-    gets(buffer); // Vulnerable function
-    printf("Hello, %s!\n", buffer);
+void vuln(){
+  char buf[BUFSIZE];
+  gets(buf);
+
+  printf("Okay, time to return... Fingers Crossed... Jumping to 0x%x\n", get_return_address());
 }
 
-int main() {
-    setbuf(stdout, NULL);
-    vault();
-    return 0;
+int main(int argc, char **argv){
+
+  setvbuf(stdout, NULL, _IONBF, 0);
+
+  gid_t gid = getegid();
+  setresgid(gid, gid, gid);
+
+  puts("Please enter your string: ");
+  vuln();
+  return 0;
 }
